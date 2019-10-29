493a210620d91131af2ea3554b7e8059adcde505
mariabrock <mariabrock414@gmail.com>
2019-10-28 19:14:37 -0500

    promises, promises

--- src/javascripts/components/cowList/cowList.js
+++ src/javascripts/components/cowList/cowList.js
@@ -3,16 +3,9 @@ import './cowList.scss';
 
 const buildCows = () => {
   cowData.getCows()
-    .then((response) => {
-      const demCows = response.data.cows;
-      const cows = [];
-      Object.keys(demCows).forEach((cowId) => {
-        // object.keys gets you an array of all the keys in the object and lets you convert them into and array
-        // array methods areeasier than object methods
-        demCows[cowId].id = cowId;
-        cows.push(demCows[cowId]);
-      });
-      console.log('it worked!', cows);
+    .then((cows) => {
+      console.log('cow array from cow list', cows);
+      //  call a domstring builder and pass in a response (cows)
     })
     .catch((error) => {
       console.error('it broke', error);
